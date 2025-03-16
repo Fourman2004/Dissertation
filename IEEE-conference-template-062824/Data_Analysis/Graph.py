24aa6238efc9309b6d5908b04b99cc13456d0181
Marshall Sharp <76953814+Fourman2004@users.noreply.github.com>
2025-03-16 21:56:32 +0000

    new Class Diagram  + Write up

--- IEEE-conference-template-062824/Data_Analysis/Graph.py
+++ IEEE-conference-template-062824/Data_Analysis/Graph.py
@@ -7,7 +7,7 @@ csv_data = pd.read_csv("data.csv")
 
 def plotData():
     sb.set_style("darkgrid")
-    sb.regplot(x = csv_data["Time"], y = csv_data["Actual Time"], data = csv_data, ci=None)
+    sb.regplot(x = csv_data["Actual Time"], y = csv_data["Time"], data = csv_data, ci=None)
     plt.show()
 
 
