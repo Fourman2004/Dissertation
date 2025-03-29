c0eb21c79e94a5bf7f4a24942d6cb88d7aa5f2c9
Marshall Sharp <76953814+Fourman2004@users.noreply.github.com>
2025-03-29 18:20:42 +0000

    Small graph Overhall

--- IEEE-conference-template-062824/Data_Analysis/Graph.py
+++ IEEE-conference-template-062824/Data_Analysis/Graph.py
@@ -7,7 +7,7 @@ csv_data = pd.read_csv("data.csv")
 
 def plotData():
     sb.set_style("darkgrid")
-    sb.regplot(x = csv_data["Hours of Sleep"], y = csv_data["Actual Time"], data = csv_data, ci=None)
+    sb.regplot(x = csv_data["Interview/Documented Time (minutes)"], y = csv_data["Time Played (minutes)"], data = csv_data, ci=None)
     plt.show()
 
 
